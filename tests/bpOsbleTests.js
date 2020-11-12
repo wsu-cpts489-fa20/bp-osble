60fecddaa83db5ef54b605165105ad491e81460f
jstallwo <joshua.stallworth@wsu.edu>
2020-11-12 10:48:35 -0800

    Starting test files

---
+++ tests/bpOsbleTests.js
@@ -0,0 +1,9 @@
+import { Selector } from 'testcafe';
+
+//Outline of what we need
+//Test Case Login Page
+//Test Case Sign Up Page
+//Test Case Forgot Password Page
+//Test Case Forgot Username Page
+
+test();
