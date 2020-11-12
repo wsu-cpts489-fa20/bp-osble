71b698aa17a06f3b37ccc25d6a631f470e6f2c0d
jstallwo <joshua.stallworth@wsu.edu>
2020-11-12 13:28:32 -0800

    Updated outline scheme, now working on case by case test

--- tests/bpOsbleTests.js
+++ tests/bpOsbleTests.js
@@ -1,3 +1,4 @@
+import { async } from 'regenerator-runtime';
 import { Selector } from 'testcafe';
 
 //Outline of what we need
@@ -6,4 +7,38 @@ import { Selector } from 'testcafe';
 //Test Case Forgot Password Page
 //Test Case Forgot Username Page
 
-test();
+fixture .page();
+
+test('LoginGoesToLoginPage', async t => {
+    await t
+        .typeText('#emailInput', 'joshua.stallworth@wsu.edu')
+        .typeText('#passwordInput', '123#abc')
+        
+});
+
+test('LoginGoesToSignUpPage', async t => {
+    await t
+        .click('#signup-button');
+
+        let articleHeader = await Selector('.result-content').find('h1');
+
+        let headerText = await articleHeader.innerText;
+});
+
+test('LoginGoesToForgotPasswordPage', async t => {
+    await t
+        .click('#forgotpassword-button');
+
+        let articleHeader = await Selector('.result-content').find('h1');
+
+        let headerText = await articleHeader.innerText;
+});
+
+test('LoginGoesToForgotUsernamePage', async t => {
+    await t
+        .click('#forgotusername-button');
+
+        let articleHeader = await Selector('.result-content').find('h1');
+
+        let headerText = await articleHeader.innerText;
+});
