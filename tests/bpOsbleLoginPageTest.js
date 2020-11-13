41a3ec03ceac8c6c04d9365efe7f109d51627344
jstallwo <joshua.stallworth@wsu.edu>
2020-11-13 11:59:30 -0800

    Second test case almost complete

--- tests/bpOsbleLoginPageTest.js
+++ tests/bpOsbleLoginPageTest.js
@@ -13,9 +13,12 @@ fixture `bp-osble`
 //This test performs a simple check to see if we are on the
 //login page
 test('LoginGoesToLoginPage', async t => {
+
+    const passwordInput = Selector('input').withAttribute('class', 'form-control enterPassword', 'type', 'password', 'placeholder', 'Password');
+
     await t
         .typeText('#emailInput', 'joshua.stallworth@wsu.edu')
-        //.typeText('#password', '123#abc;')
+        .typeText(passwordInput, '123#abc;')
         // .click('#login-btn-icon');
         const articleHeader = await Selector('https://i.imgur.com/lBOwYfo.png').find('div')
         let headerText = await articleHeader.withAttribute;
--- tests/bpOsbleSignUpTest.js
+++ tests/bpOsbleSignUpTest.js
@@ -5,6 +5,33 @@ fixture `bp-osble`
 .page `http://localhost:8081`;
 
 test('LoginPageGoesToSignUpPage', async t => {
+
+    //testing variables
+    const accountBtn = Selector('button').withAttribute('type', 'button', 'class', 'btn btn-link login-link', 'Create an account');
+    const emailSignUp = Selector('input').withAttribute('name', 'email');
+    const emailComf = Selector('input').withAttribute('name', 'confirm_email');
+    const passwordSignUp = Selector('input').withAttribute('class', 'form-control form-text form-center', 'name', 'password', 'type', 'password');
+    const passwordComf = Selector('input').withAttribute('name', 'confirm_password');
+    const firstName = Selector('input').withAttribute('name', 'first_name');
+    const lastName = Selector('input').withAttribute('name', 'last_name');
+    const lastNameComf = Selector('input').withAttribute('name', 'confirm_last_name');
+    const firstNameComf = Selector('input').withAttribute('name', 'confirm_first_name');
+    const schoolInfo = Selector('select').withAttribute('name', 'school', 'class', 'form-control form-center');
+    const schoolID = Selector('input').withAttribute('name', 'id', 'type', 'number');
+    const schoolIDComf = Selector('input').withAttribute('name', 'confirm_id', 'type', 'number');
+    const registerBtn = Selector('button').withAttribute('class', 'register-btn', 'role', 'submit', 'type', 'submit');
+
     await t
-        .click(Create-an-account);
+        .click(accountBtn)
+        .typeText(emailSignUp,'joshua.stallworth@wsu.edu')
+        .typeText(emailComf,'joshua.stallworth@wsu.edu')
+        .typeText(passwordSignUp,'password123#abc;')
+        .typeText(passwordComf,'password123#abc;')
+        .typeText(firstName,'Josh')
+        .typeText(lastName,'Stallworth')
+        .typeText(lastNameComf,'Stallworth')
+        .typeText(firstNameComf,'Josh')
+        .typeText(schoolID,'1234567890')
+        .typeText(schoolIDComf,'1234567890')
+        .click(registerBtn);
 });
