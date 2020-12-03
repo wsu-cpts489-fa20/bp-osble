15734c950eb632d64085ba210e2d4b69fb0b111a
jstallwo <joshua.stallworth@wsu.edu>
2020-12-03 15:45:36 -0800

    Updated schemas, adding more for different related course pages

--- server/course.js
+++ server/course.js
@@ -1,9 +1,24 @@
+const { Schema } = require("mongoose");
+const nodemon = require("nodemon");
+const { t } = require("testcafe");
 
-const courseSchema = new Schema({
-    courseName: String, // unique identifier
+
+const courseSettingsSchema = new Schema({
+    prefix: { type: String, required: true },
+    courseNumber: { type: Number, required: true },
+    courseName: { type: String, require: true },
+    courseTerm: { type: String, required: true },
+    courseYear: { type: Number, required: true },
+    courseStartDate: { type: Number, required: true },
+    courseEndDate: { type: Number, required: true },
+    timeZone: { type: String, required: true },
     post: [],
     instructor: String,
     Assignments: []
                 
 })
 
+const courseHomeSchema = new Schema({
+    post: { type: String, minlength: 1, maxlength: 520}
+    //need reply post data?
+})
